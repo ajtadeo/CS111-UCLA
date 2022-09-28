a6c74f2c157581f9185e8dd736b13a14d8988b7e
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-09-28 11:31:58 -0700

    proc_create_single implementation

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -5,18 +5,27 @@
 #include <linux/sched.h>
 
 // global variables
-static int count;
+static int numLive;
+static struct proc_dir_entry *count;
 
+// helper functions implementation
+static int proc_count_show(struct seq_file *m, void *v){
+	seq_printf(m, "Hello, world!")
+	return 0;
+}
+
+// init and exit functions
 static int __init proc_count_init(void)
 {
 	pr_info("proc_count: init\n");
-	count = 0;
-	pr_info("Count : %d\n", count);
+	numLive = 0;
+	count = proc_create_single("count", 0, NULL, proc_count_show);
 	return 0;
 }
 
 static void __exit proc_count_exit(void)
 {
+	proc_remove(count);
 	pr_info("proc_count: exit\n");
 }
 
