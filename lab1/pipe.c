7411748cfcab35bbefa912e39533e84832c988de
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-10-07 21:11:34 -0700

    pipe for one argument

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -1,4 +1,34 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[])
 {
+	// exit if there's an incorrect number of args
+	if (argc == 0){
+		exit(EINVAL);
+	}
+	// first set up the pipe file descriptors for the parent process
+	// pipefd[0] = read end
+	// pipefd[1] = write end
+	// int pipefd[2];
+	// int pipe(pipefd);
+
+	// fork to call the first pipe command
+	int pid = fork();
+	if (pid>0){
+		printf("Parent process\n");
+		int status = 0;
+		waitpid(pid, &status, 0);
+	} else if (pid==0){
+		printf("Child process: %d", pid);
+		execlp(argv[1], NULL);
+		exit(0);
+	} else {
+		exit(ECHILD);
+	}
+
 	return 0;
 }
