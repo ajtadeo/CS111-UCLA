30d8d46e8534066bc82ee39f10fe6d6e6ed03309
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-10-11 23:07:14 -0700

    one pipe implementation

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -8,28 +8,67 @@ int main(int argc, char *argv[])
 {
 	// exit if there's an incorrect number of args
 	if (argc <= 1){
-		printf("Error: Incorrect number of variables\n");
+		printf("Error %d: Incorrect number of variables\n", EINVAL);
 		exit(EINVAL);
 	}
-	// first set up the pipe file descriptors for the parent process
-	// pipefd[0] = read end
-	// pipefd[1] = write end
-	// int pipefd[2];
-	// int pipe(pipefd);
 
-	// fork to call the first pipe command
+	// set up pipe organization for all processes
+	int *pipes[8]; // holds all pipes in the program
+	
+	// set up pipe and fork for the first and second processes
+	int pipefd[2]; // pipefd_n[0] = read end, pipefd_n[1] = write end
+	int pipe(pipefd);
+	pipes[0] = &pipefd;
+
+	// setup for first process
 	int pid = fork();
-	if (pid>0){
-		printf("Parent process\n");
-		int status = 0;
-		waitpid(pid, &status, 0);
-	} else if (pid==0){
-		printf("Child process: %d", pid);
-		execlp(argv[1], argv[1], NULL);
-		exit(0);
-	} else {
-		exit(ECHILD);
+	dup2(pipefd[1], STDOUT_FILENO);
+	for (int i=1; i<argc; i++){
+		switch(pid){
+			case 0: 
+				// child process
+				printf("Child process: %d", pid);
+				// organize pipe
+				if (dup2(pipefd[1], STDIN_FILENO) == -1){
+					printf("Error %d: dup2() failed.", errno);
+					exit(errno);
+				}
+				close(pipefd[1]);
+				close(pipefd[0]); // unused write
+				// execute the command
+				int status = execlp(argv[1], argv[1], NULL);
+				if (status == -1){
+					printf("Error %d: Command execution failed", errno);
+					exit(errno);
+				}
+				exit(0);
+				break;
+			case -1:
+				// error occurred
+				printf("Error %d: Child creation unsuccessful", ECHILD);
+				exit(ECHILD);
+				break;
+			default:
+				// parent process
+				printf("Parent process\n");
+				// organize pipe
+				if (dup2(pipefd[0], STDOUT_FILENO) == -1){
+					printf("Error %d: dup2() failed.", errno);
+					exit(errno);
+				}
+				close(pipefd[0]);
+				close(pipefd[1]); // unused read
+				// wait for child execution
+				int status = 0;
+				if (waitpid(pid, &status, 0) < 0){
+					printf("Error %d: waitpid() failed.", errno);
+					exit(errno);
+				}
+				if (!WIFEXITED(status) || WEXITSTATUS(status) != 0){
+					exit(errno);
+				}
+				break;
+		}
 	}
 
 	return 0;
-}
