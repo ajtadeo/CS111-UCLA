f82158f423ed304c8e335fccf3c4d02762cbe824
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-11-26 12:27:38 -0800

    fix superblock init values

--- lab4/ext2-create.c
+++ lab4/ext2-create.c
@@ -21,21 +21,15 @@ typedef int32_t i32;
 #define NUM_INODES 128
 
 // tadeo
-#define R_BLOCKS_COUNT 0
-#define FIRST_DATA_BLOCK 0 /* larger than 1 KB = 0 */
-#define LOG_BLOCK_SIZE 0 /* size of block as a power of 2: 2^0 = 1024 */
-#define LOG_FRAG_SIZE 0 /* size of frag as a power of 2: 2^0 = 1024 */
-#define INIT_MNT_COUNT 0
-#define BLOCKS_PER_GROUP (BLOCK_SIZE * 8)
 #define EXT2_SUPER_MAGIC 0xEF53
 #define EXT2_VALID_FS 1			// Unmounted cleanly
 #define EXT2_ERROR_FS 2			// Errors detected
 #define EXT2_ERRORS_CONTINUE 1	// continue as if nothing happened
-#define EXT2_OS_LINUX 0
-#define ROOT_UID 0
-#define ROOT_GID 0
 #define CHECK_INTERVAL 1
+#define UNLIMITED_MNT_COUNT 9999
 
+// inode 2 is reserved for the root directory
+// first 10 inodes are reserved
 #define LOST_AND_FOUND_INO 11
 #define HELLO_WORLD_INO    12
 #define HELLO_INO          13
@@ -214,29 +208,29 @@ void write_superblock(int fd) {
 	   correctly and delete this comment */
 	superblock.s_inodes_count      = NUM_INODES;
 	superblock.s_blocks_count      = NUM_BLOCKS;
-	superblock.s_r_blocks_count    = R_BLOCKS_COUNT; // blocks that cannot be written to
+	superblock.s_r_blocks_count    = 0; // blocks that cannot be written to
 	superblock.s_free_blocks_count = NUM_FREE_BLOCKS;
 	superblock.s_free_inodes_count = NUM_FREE_INODES;
-	superblock.s_first_data_block  = FIRST_DATA_BLOCK; 
-	superblock.s_log_block_size    = LOG_BLOCK_SIZE; 
-	superblock.s_log_frag_size     = LOG_FRAG_SIZE; 
-	superblock.s_blocks_per_group  = BLOCKS_PER_GROUP; // one group in the fs
-	superblock.s_frags_per_group   = 0; // zeroed out
-	superblock.s_inodes_per_group  = 0;
+	superblock.s_first_data_block  = SUPERBLOCK_BLOCKNO; // location of the super block
+	superblock.s_log_block_size    = 0; /* size of block as a power of 2: 2^0 = 1024 */
+	superblock.s_log_frag_size     = 0; /* size of frag as a power of 2: 2^0 = 1024 */
+	superblock.s_blocks_per_group  = (BLOCK_SIZE * 8); // one group in the fs, max num represented in bitmap
+	superblock.s_frags_per_group   = (BLOCK_SIZE * 8); // one group in the fs, max num represented in bitmap
+	superblock.s_inodes_per_group  = NUM_INODES; // one group in the fs
 	superblock.s_mtime             = current_time; /* Mount time */
 	superblock.s_wtime             = current_time; /* Write time */
-	superblock.s_mnt_count         = INIT_MNT_COUNT; /* Number of times mounted so far */
-	superblock.s_max_mnt_count     = 1; /* Make this unlimited */
+	superblock.s_mnt_count         = 0; /* Number of times mounted so far */
+	superblock.s_max_mnt_count     = UNLIMITED_MNT_COUNT; /* Make this unlimited */
 	superblock.s_magic             = EXT2_SUPER_MAGIC; /* ext2 Signature */
 	superblock.s_state             = EXT2_VALID_FS; /* File system is clean */
 	superblock.s_errors            = EXT2_ERRORS_CONTINUE; /* Ignore the error (continue on) */
 	superblock.s_minor_rev_level   = 0; /* Leave this as 0 */
 	superblock.s_lastcheck         = current_time; /* Last check time */
 	superblock.s_checkinterval     = CHECK_INTERVAL; /* Force checks by making them every 1 second */
-	superblock.s_creator_os        = EXT2_OS_LINUX; /* Linux */
+	superblock.s_creator_os        = 0; /* Linux */
 	superblock.s_rev_level         = 0; /* Leave this as 0 */
-	superblock.s_def_resuid        = ROOT_UID; /* root */
-	superblock.s_def_resgid        = ROOT_GID; /* root */
+	superblock.s_def_resuid        = 0; /* root user id */
+	superblock.s_def_resgid        = 0; /* root group id */
 
 	/* You can leave everything below this line the same, delete this
 	   comment when you're done the lab */
